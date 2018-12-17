6522614961c5602c1f91489e03d648ee16f03180
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2018-12-17 16:53:46 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,7 +1,10 @@
 class Owner
   attr_accessor :species
   def initialize
-    @species = human
+    @species = "human"
     @species.freeze
   end
 end
+Tom = Owner.new
+puts Tom.species
+Tom.species = "hamster"
