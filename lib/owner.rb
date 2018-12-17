1b683c3d8b12cf01e389d9eeb41337c42e6ff91b
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2018-12-17 17:18:13 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -37,6 +37,16 @@ class Owner
       fish.mood = "happy"
     end
   end
+  def play_with_cats
+    @pets[:cats].each do |cat|
+      cat.mood = "happy"
+    end 
+  end 
+  def walk_dogs
+    @pets[:dogs].each do |dog|
+      dog.mood = "happy"
+    end 
+  end         
 end
 Tom = Owner.new("tom")
 Tom.buy_fish("Akuma")
